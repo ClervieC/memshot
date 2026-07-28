31745537f294e4fbbe2d3375e4ea74e0161f8d79
ClervieCauser <168409522+ClervieC@users.noreply.github.com>
2026-07-28 22:03:42 +0300

    v2.0.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   supabase: {
-    url: 'https://mwjhxlggggcgwgxedaps.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13amh4bGdnZ2djZ3dneGVkYXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MDgyMTQsImV4cCI6MjEwMDI4NDIxNH0.lXg4LQwD1G_3ar5kQeVqddFzuKYEiZ7GFDZ8c09MQ1E',
+    url: 'https://memshot-supabase.duckdns.org',
+    anonKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH',
     mediaBucket: 'memshot-media',
   },
 };
